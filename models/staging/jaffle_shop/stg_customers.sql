d23e31c301c8627ffee5c98929a3aa02f1f6d33f
Janerose Njogu <janerosenyams@gmail.com>
2023-02-13 09:42:13 +0000

    added source files and tested source freshness

--- models/staging/jaffle_shop/stg_customers.sql
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -1,6 +1,3 @@
-select
-    id as customer_id,
-    first_name,
-    last_name
+select id as customer_id, first_name, last_name
 
-from raw.jaffle_shop.customers
+from {{ source("jaffle_shop", "customers") }}
--- models/staging/jaffle_shop/stg_orders.sql
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -1,7 +1,3 @@
-select
-    id as order_id,
-    user_id as customer_id,
-    order_date,
-    status
+select id as order_id, user_id as customer_id, order_date, status
 
-from raw.jaffle_shop.orders
+from {{ source("jaffle_shop", "orders") }}
